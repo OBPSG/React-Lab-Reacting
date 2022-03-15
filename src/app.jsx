56b4f15ef4366315b27af86aa5e5697eba1e2142
OBPSG <vbeasupergamer@gmail.com>
2022-03-15 17:58:25 -0500

    Finished step 10 of Simple Tasks

--- src/app.jsx
+++ src/app.jsx
@@ -1,32 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Greeter from './components/Greeter';
 
 const App = () => {
-    const [username, setUsername] = useState('')
+    const [username, setUsername] = useState('');
     const [loaded, setLoaded] = useState(false);
 
-    if(loaded)
-    {
-    return (
+    useEffect(() => {
+        setTimeout(() => {
+            if (!loaded) {
+                setLoaded(true);
+            }
+        }, 5000);
+    }, [loaded]);
+
+    if (loaded) {
+        return (
             <div>
                 <h1>Hello World, from React!</h1>
-                <Greeter phrase="Hello again" name ="World"></Greeter>
+                <Greeter phrase="Hello again" name="World"></Greeter>
                 <Greeter phrase="Nice to meet you" name="React"></Greeter>
                 <Greeter phrase="It's a lovely day to learn web development" name="isn't it?"></Greeter>
-                <input value={username} onChange={ (event) => setUsername(event.target.value)}></input>
+                <input value={username} onChange={(event) => setUsername(event.target.value)}></input>
                 <p>You are logging in as {username}</p>
             </div>
         );
     }
-    else{
+    else {
         return (
-        <>
-            <h1>Loading...</h1>
-            <button onClick={() => {setLoaded(true)}}>Hurry up!</button>
-        </>
-        );     
+            <>
+                <h1>Loading...</h1>
+                <button onClick={() => { setLoaded(true) }}>Hurry up!</button>
+            </>
+        );
     }
-    
+
 };
 
 export default App
