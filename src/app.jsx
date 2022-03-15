201e1dd2e3dd066dc9cd0732c3f6f88f30af8b3a
OBPSG <vbeasupergamer@gmail.com>
2022-03-15 17:44:00 -0500

    Completed steps 4-6 of Simple Tasks

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Greeter from './components/Greeter';
 
 const App = () => {
+    const [username, setUsername] = useState('')
+    const [loaded, setLoaded] = useState(false);
+
+    if(loaded)
+    {
     return (
-        <div>
-            <h1>Hello World, from React!</h1>
-            <Greeter phrase="Hello again" name ="World"></Greeter>
-            <Greeter phrase="Nice to meet you" name="React"></Greeter>
-            <Greeter phrase="It's a lovely day to learn web development" name="isn't it?"></Greeter>
-        </div>
-    );
+            <div>
+                <h1>Hello World, from React!</h1>
+                <Greeter phrase="Hello again" name ="World"></Greeter>
+                <Greeter phrase="Nice to meet you" name="React"></Greeter>
+                <Greeter phrase="It's a lovely day to learn web development" name="isn't it?"></Greeter>
+                <input value={username} onChange={ (event) => setUsername(event.target.value)}></input>
+                <p>You are logging in as {username}</p>
+            </div>
+        );
+    }
+    else{
+        return (
+        <>
+            <h1>Loading...</h1>
+            <button onClick={() => {setLoaded(true)}}>Hurry up!</button>
+        </>
+        );     
+    }
+    
 };
 
 export default App
